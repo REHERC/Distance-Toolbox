a1f80babed834a03886474159e974f9b8d4e1dda
REHERC <amaury.vanoorenberghe@yahoo.com>
2018-11-21 22:13:23 +0100

    Added task list

---
+++ Photon/TODO.cs
@@ -0,0 +1,5 @@
+﻿/*
+TODO: Add Cal1fornia's playlist editor
+TODO: Add ferreum's distanceutils python library (embedded python for .net too)
+TODO: Add query system to gather online servers from www.distance.rip
+*/
